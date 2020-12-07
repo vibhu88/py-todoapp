6c4ab29aeda76d8a3695c7ae8fb3de386c884796
vibhu88 <vibhu.eclipse@gmail.com>
2020-12-07 12:23:28 +0530

    Commit till lession 2.6 Migrations

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/fsnd'
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed   = db.Column(db.Boolean, nullable=True, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+# db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.form.get('description', '')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all())
